1c9b659c395f63c6b9b09c7f5f111f2e08e65351
GenaCrok <gennadiyemelin92@gmail.com>
2025-03-20 22:36:24 +0300

    second commit.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DotNet
 {
@@ -10,6 +11,38 @@ namespace DotNet
     {
         static void Main(string[] args)
         {
+            SqlConnectionStringBuilder strCon = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(localdb)\MAAQLlocalDB",
+                InitialCatalog = "MSSQLTestDemo",
+                IntegratedSecurity = true
+            };
+
+            Console.WriteLine(strCon.ConnectionString);
+
+            SqlConnection sqlConnection = new SqlConnection()
+            {
+                ConnectionString = strCon.ConnectionString,
+            };
+
+            sqlConnection.StateChange += (s, e) => 
+            { Console.WriteLine($"{nameof(sqlConnection)} " +
+                $"в состоянии: {(s as SqlConnection).State}"); 
+            };
+
+            try
+            {
+                sqlConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(sqlConnection.State);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
         }
     }
 }
